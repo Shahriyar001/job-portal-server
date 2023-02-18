b3a189b8c54d78d4f52ad24ff79ed8a466aa08dd
Shahriyar001 <shahriyarmahbub55@gmail.com>
2023-02-18 13:21:59 +0600

    collect data from database

--- index.js
+++ index.js
@@ -11,11 +11,40 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dawimtn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        const freshersCollection = client.db('jobPortal').collection('freshers');
+        const experienceCollection = client.db('jobPortal').collection('experience');
+        const companiesCollection = client.db('jobPortal').collection('companies');
+
+        app.get('/freshers', async (req, res) => {
+            const query = {}
+            const cursor = freshersCollection.find(query);
+            const freshers = await cursor.toArray();
+            res.send(freshers);
+
+        })
+
+        app.get('/experience', async (req, res) => {
+            const query = {}
+            const cursor = experienceCollection.find(query);
+            const experience = await cursor.toArray();
+            res.send(experience);
+        })
+
+        app.get('/companies', async (req, res) => {
+            const query = {}
+            const cursor = companiesCollection.find(query);
+            const companies = await cursor.toArray();
+            res.send(companies);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
 
 
 app.get('/', (req, res) => {
